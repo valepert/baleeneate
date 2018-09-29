b308d951ad116b08cce033b532f4215d9019bb5d
valepert <viX666@gmail.com>
2018-09-29 18:05:05 +0200

    Better flags

--- spla.sh
+++ spla.sh
@@ -5,4 +5,4 @@ CONTAINER_NAME=baleeneate
 # works on Mac
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run --rm --name baleeneate -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix valepert/baleeneate
+docker run --rm --name $CONTAINER_NAME -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix -d valepert/baleeneate
