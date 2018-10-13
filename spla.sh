29c1ec3b7c9fecf3b23a33fd679d1e2d7e03e126
valepert <viX666@gmail.com>
2018-10-13 19:27:56 +0200

    Docker Compose file

--- spla.sh
+++ spla.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME=baleeneate
 
-# works on Mac
+# works on Mac (XQuartz needed)
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run --rm --name $CONTAINER_NAME -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix -d valepert/baleeneate
+DISPLAY=${IP}:0 docker-compose up
